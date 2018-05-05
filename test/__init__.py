828088a54b84aa904ec54c50af60f8aecc62a5f4
cpburnz <cpburnz@gmail.com>
2018-05-04 21:04:25 -0400

    Use assertEqual

--- test/__init__.py
+++ test/__init__.py
@@ -54,8 +54,8 @@ class SQLParamsTest(unittest.TestCase):
 		sql, params = query.format(src_sql, src_params)
 
 		# Make sure desired SQL and parameters are created.
-		self.assertEquals(sql, dest_sql)
-		self.assertEquals(params, dest_params)
+		self.assertEqual(sql, dest_sql)
+		self.assertEqual(params, dest_params)
 
 	def test_00_numeric_to_qmark(self):
 		"""
@@ -93,8 +93,8 @@ class SQLParamsTest(unittest.TestCase):
 			sql, params = query.format(src_sql, src_params)
 
 			# Make sure desired SQL and parameters are created.
-			self.assertEquals(sql, dest_sql)
-			self.assertEquals(params, dest_params)
+			self.assertEqual(sql, dest_sql)
+			self.assertEqual(params, dest_params)
 
 	def test_00_pyformat_to_qmark(self):
 		"""
@@ -129,8 +129,8 @@ class SQLParamsTest(unittest.TestCase):
 		sql, params = query.format(src_sql, src_params)
 
 		# Make sure desired SQL and parameters are created.
-		self.assertEquals(sql, dest_sql)
-		self.assertEquals(params, dest_params)
+		self.assertEqual(sql, dest_sql)
+		self.assertEqual(params, dest_params)
 
 	def test_00_named_to_format(self):
 		"""
@@ -165,8 +165,8 @@ class SQLParamsTest(unittest.TestCase):
 		sql, params = query.format(src_sql, src_params)
 
 		# Make sure desired SQL and parameters are created.
-		self.assertEquals(sql, dest_sql)
-		self.assertEquals(params, dest_params)
+		self.assertEqual(sql, dest_sql)
+		self.assertEqual(params, dest_params)
 
 	def test_01_tuple(self):
 		"""
@@ -201,8 +201,8 @@ class SQLParamsTest(unittest.TestCase):
 		sql, params = query.format(src_sql, src_params)
 
 		# Make sure desired SQL and parameters are created.
-		self.assertEquals(sql, dest_sql)
-		self.assertEquals(params, dest_params)
+		self.assertEqual(sql, dest_sql)
+		self.assertEqual(params, dest_params)
 
 	def test_02_many(self):
 		"""
@@ -245,8 +245,8 @@ class SQLParamsTest(unittest.TestCase):
 		sql, many_params = query.formatmany(src_sql, src_params)
 
 		# Make sure desired SQL and parameters are created.
-		self.assertEquals(sql, dest_sql)
-		self.assertEquals(many_params, dest_params)
+		self.assertEqual(sql, dest_sql)
+		self.assertEqual(many_params, dest_params)
 
 	def test_02_many_tuple(self):
 		"""
@@ -292,8 +292,8 @@ class SQLParamsTest(unittest.TestCase):
 		sql, many_params = query.formatmany(src_sql, src_params)
 
 		# Make sure desired SQL and parameters are created.
-		self.assertEquals(sql, dest_sql)
-		self.assertEquals(many_params, dest_params)
+		self.assertEqual(sql, dest_sql)
+		self.assertEqual(many_params, dest_params)
 
 	def test_03_encode_bytes(self):
 		"""
@@ -301,7 +301,7 @@ class SQLParamsTest(unittest.TestCase):
 		"""
 		encoded = "".join(map(unichr, range(0,256))).encode(sqlparams._BYTES_ENCODING)
 		expected = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
-		self.assertEquals(encoded, expected)
+		self.assertEqual(encoded, expected)
 
 	def test_03_decode_bytes(self):
 		"""
@@ -309,7 +309,7 @@ class SQLParamsTest(unittest.TestCase):
 		"""
 		decoded = bytes(bytearray(range(0,256))).decode(sqlparams._BYTES_ENCODING)
 		expected = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
-		self.assertEquals(decoded, expected)
+		self.assertEqual(decoded, expected)
 
 	def test_04_format_bytes(self):
 		"""
@@ -338,8 +338,8 @@ class SQLParamsTest(unittest.TestCase):
 		sql, params = query.format(src_sql, src_params)
 
 		# Make sure desired SQL and parameters are created.
-		self.assertEquals(sql, dest_sql)
-		self.assertEquals(params, dest_params)
+		self.assertEqual(sql, dest_sql)
+		self.assertEqual(params, dest_params)
 
 	def test_04_formatmany_bytes(self):
 		"""
@@ -368,5 +368,5 @@ class SQLParamsTest(unittest.TestCase):
 		sql, many_params = query.formatmany(src_sql, src_params)
 
 		# Make sure desired SQL and parameters are created.
-		self.assertEquals(sql, dest_sql)
-		self.assertEquals(many_params, dest_params)
+		self.assertEqual(sql, dest_sql)
+		self.assertEqual(many_params, dest_params)
